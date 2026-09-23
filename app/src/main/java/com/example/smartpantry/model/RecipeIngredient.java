5f3e8c429e6e4cd02f386b7b178117536f17f797
BridgetM <402501415@my.richfield.ac.za>
2026-09-23 14:10:50 +0200

    Feature - Final touch

---
+++ app/src/main/java/com/example/smartpantry/model/RecipeIngredient.java
@@ -0,0 +1,64 @@
+package com.example.smartpantry.model;
+
+/**
+ * A single ingredient requirement belonging to a Recipe.
+ */
+public class RecipeIngredient {
+
+    private long id;
+    private long recipeId;
+    private String name;
+    private double quantity;
+    private String unit;
+
+    public RecipeIngredient() {
+    }
+
+    public RecipeIngredient(long id, long recipeId, String name, double quantity, String unit) {
+        this.id = id;
+        this.recipeId = recipeId;
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
